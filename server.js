bb39a86db75f7e4d807a2d2c6991ee6bd6df5899
evac62 <evac62@gmail.com>
2016-02-22 10:20:11 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("It's alive!");
   response.end();
 }).listen(3000);
+
