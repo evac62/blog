2de48d536ceeaf4a7bd3f70ebda1d6e424987fe8
evac62 <evac62@gmail.com>
2016-02-22 10:47:31 -0600

    Update server.js

--- server.js
+++ server.js
@@ -9,3 +9,47 @@ http.createServer(function(request, response) {
   response.end();
 }).listen(3000);
 
+function send404(response) {
+  response.writeHead(404, {"Content-type" : "text/plain"});
+  response.write("Error 404: resource not found");
+  response.end();
+}
+
+function sendPage(response, filePath, fileContents) {
+  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+  response.end(fileContents);
+}
+
+function serverWorking(response, absPath) {
+  fs.exists(absPath, function(exists) {
+    if (exists) {
+      fs.readFile(absPath, function(err, data) {
+        if (err) {
+          send404(response)
+        } else {
+          sendPage(response, absPath, data);
+        }
+      });
+    } else {
+      send404(response);
+    }
+  });
+}
+
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  if (request.url == '/') {
+    filePath = "public/index.html";
+  } else {
+    filePath = "public" + request.url;
+  }
+
+  var absPath = "./" + filePath;
+  serverWorking(response, absPath);
+});
+
+http.createServer(<some code here>).listen(3000)
+
+var port_number = server.listen(process.env.PORT || 3000);
+
